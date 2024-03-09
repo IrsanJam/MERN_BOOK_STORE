6620c8ef5abbe30b653f25ac982b2feb057fffc6
Irsan Jamaludin <114846911+IrsanJam@users.noreply.github.com>
2024-03-10 00:02:54 +0700

    Deploy (#11)
    * update json
    
    * update folder-frontend
    
    * add vercel.json
    
    * resolve: cors
    
    * add update vercel.json
    
    * update
    
    * update api
    
    * update credentials
    
    * update data
    
    * update data
    
    * update dataku

--- frontend/src/pages/ShowBook.jsx
+++ frontend/src/pages/ShowBook.jsx
@@ -15,7 +15,7 @@ const ShowBook = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get(`${baseUrl}/${id}`)
+      .get(`${baseUrl}books/${id}`)
       .then((response) => {
         setBook(response.data);
         setLoading(false);
