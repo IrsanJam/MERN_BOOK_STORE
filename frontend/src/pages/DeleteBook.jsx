a8c4d4387336b03300affe7298142f692529f7ca
Irsan Jamaludin <114846911+IrsanJam@users.noreply.github.com>
2024-03-11 17:46:18 +0700

    feat: responsive design (#13)
    * update json
    
    * update folder-frontend
    
    * add vercel.json
    
    * resolve: cors
    
    * add update vercel.json
    
    * update
    
    * update api
    
    * update credentials
    
    * update data
    
    * update data
    
    * update dataku
    
    * add update
    
    * update data

--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -34,8 +34,8 @@ const DeleteBook = () => {
       <BackButton />
       <h1 className="text-3xl my-4">Delete Book</h1>
       {loading ? <Spinner /> : ""}
-      <div className="flex flex-col items-center border-2 border-sky-400 rounded-xl w-[600px] p-8 mx-auto">
-        <h3 className="text-2xl">Are You Sure You want to delete this book?</h3>
+      <div className="flex flex-col items-center border-2 mt-[10vh] border-sky-400 rounded-xl md:w-[600px] w-full p-8 gap-5 mx-auto">
+        <h3 className="md:text-2xl text-lg font-semibold">Are You Sure You want to delete this book?</h3>
         <button className="p-4 bg-red-600 text-white w-full" onClick={handleDeleteBook}>
           Yes, Delete it
         </button>
